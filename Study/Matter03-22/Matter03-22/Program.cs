e723d566f95c3c19cb797dad52beb44dbbd98732
fpsgo7 <101778043+fpsgo7@users.noreply.github.com>
2023-03-22 01:11:40 +0900

    속도 측정 프로젝트 테스트 완료
    parse 속도 비교 도전해보기

---
+++ Study/Matter03-22/Matter03-22/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+
+namespace Matter03_22
+{
+    [MemoryDiagnoser]
+    public class test1
+    {
+        int a = 1;
+        int b = 2;
+        [Benchmark]
+        public int hello()
+        {
+            return a + b;
+        }
+    }
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            var summery = BenchmarkRunner.Run<test1>();
+        }
+    }
+}
